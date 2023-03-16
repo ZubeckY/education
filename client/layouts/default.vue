2ba8fdeb66be33d70ce62c4d8cb901ad1595e746
oleg <oleg_zub00@mail.ru>
2023-03-16 12:24:47 +0300

    client-ready

---
+++ client/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+
+</template>
+<script lang="ts">
+import {Component, Vue, Prop, Watch} from "vue-property-decorator"
+export default class Default extends Vue {
+
+}
+</script>
+
---
+++ client/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script lang="ts">
+import {Component, Vue, Prop, Watch} from "vue-property-decorator"
+export default class Pages extends Vue {
+
+}
+</script>
