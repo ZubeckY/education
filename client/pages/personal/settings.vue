5f0c921183c79df84156e68e22070144a2141d77
oleg <oleg_zub00@mail.ru>
2023-03-29 20:09:14 +0300

    fix-values

--- client/pages/personal/settings.vue
+++ client/pages/personal/settings.vue
@@ -7,79 +7,82 @@
            5. Color   |
            -->
   <v-container>
-    <v-text-field
-      label="Имя"
-      v-model="params.name"
-      filled
-    ></v-text-field>
+    <v-card class="pa-2">
+      <v-text-field
+        label="Имя"
+        v-model="params.name"
+        filled
+      ></v-text-field>
 
-    <v-text-field
-      label="Фамилия"
-      v-model="params.secondName"
-      filled
-    ></v-text-field>
+      <v-text-field
+        label="Фамилия"
+        v-model="params.secondName"
+        filled
+      ></v-text-field>
 
-    <v-text-field
-      label="Фото профиля"
-      v-model="params.img"
-      filled
-    ></v-text-field>
+      <v-text-field
+        label="Фото профиля"
+        v-model="params.img"
+        filled
+      ></v-text-field>
 
-    <v-checkbox v-model="generalPhoto"
+      <v-checkbox v-model="generalPhoto"
 
-       label="Общий фон"
+                  label="Общий фон"
 
-    ></v-checkbox>
+      ></v-checkbox>
 
-    <v-text-field
-      :disabled="generalPhoto"
-      label="Бэкграунд"
-      v-model="params.background"
-      filled
-    ></v-text-field>
+      <v-text-field
+        :disabled="generalPhoto"
+        label="Бэкграунд"
+        v-model="params.background"
+        filled
+      ></v-text-field>
 
-    <v-dialog
-      v-model="dialog"
-      width="300"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          :color="params.color"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
-          Изменить цвет
-        </v-btn>
-      </template>
-
-      <v-card>
-
-        <v-color-picker
-          dot-size="22"
-          v-model="params.color"
-          mode="rgba"
-          swatches-max-height="180"
-        ></v-color-picker>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
+      <v-dialog
+        v-model="dialog"
+        width="300"
+      >
+        <template v-slot:activator="{ on, attrs }">
           <v-btn
-            color="primary"
-            text
-            @click="dialog = false"
+            :color="params.color"
+            dark
+            v-bind="attrs"
+            v-on="on"
           >
-            Изменить
+            Изменить цвет
           </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+        </template>
 
-    <v-btn>
-      Сохранить
-    </v-btn>
+        <v-card>
 
+          <v-color-picker
+            dot-size="22"
+            v-model="params.color"
+            mode="rgba"
+            swatches-max-height="180"
+          ></v-color-picker>
 
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              text
+              @click="dialog = false"
+            >
+              Изменить
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-card>
+
+    <v-card-actions>
+      <v-spacer />
+      <v-btn>
+        Сохранить
+      </v-btn>
+    </v-card-actions>
 
   </v-container>
 </template>
@@ -87,24 +90,23 @@
 import {Component, Vue, Prop, Watch} from "vue-property-decorator"
 @Component
 export default class Settings extends Vue {
-
-  params :any = {
-    name:"",
-    secondName:"",
-    img:"",
-    background:"",
-    color:"",
+  params: any = {
+    name: "",
+    secondName: "",
+    img: "",
+    background: "",
+    color: "",
   }
 
   generalPhoto: boolean = true
   dialog: boolean = false
 
-  created(){
-    this.params = this.$store.state.user
+  created () {
+    this.params = JSON.parse(JSON.stringify(this.$store.state.user))
   }
 
-  @Watch("params.color")
-  changeBackground(){
+  @Watch ("params.color")
+  changeBackground () {
     console.log("Back")
   }
 
