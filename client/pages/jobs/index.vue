8bf5f8b6869b00b9db59e72a55f51ef747fc7ba2
oleg <oleg_zub00@mail.ru>
2023-04-04 19:47:48 +0300

    Разработка работы старт

--- client/pages/jobs/index.vue
+++ client/pages/jobs/index.vue
@@ -3,7 +3,9 @@
 </template>
 <script lang="ts">
 import {Component, Vue} from "vue-property-decorator"
-@Component
+@Component ({
+  layout: 'jobs'
+})
 export default class Jobs extends Vue {
 }
 </script>
