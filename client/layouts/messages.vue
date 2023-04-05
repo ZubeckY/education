3abb936f5e6bc294375ca52c62366d18640b2aef
oleg <oleg_zub00@mail.ru>
2023-04-05 19:20:09 +0300

    messager

---
+++ client/layouts/messages.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <main-header/>
+    Сообщения
+    <nuxt/>
+  </v-app>
+</template>
+<script lang="ts">
+import {Component, Vue, Prop, Watch} from "vue-property-decorator"
+@Component
+export default class Messages extends Vue {
+
+}
+</script>
+
