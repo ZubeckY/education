3e1d0e8fd1080deedff6c8ab8b1f064d7a4a5709
oleg <oleg_zub00@mail.ru>
2023-04-06 17:57:02 +0300

    roles admin

--- client/pages/admin/roles.vue
+++ client/pages/admin/roles.vue
@@ -1,12 +1,91 @@
 <template>
-  <div>
-    Roles
-  </div>
+  <v-card elevation="0">
+    <v-card-title>Роли</v-card-title>
+
+    <v-data-table :items="roles"
+                  :headers="headers"
+                  :items-per-page="-1"
+                  class="custom-table"
+                  height="calc(100vh - 90px)"
+                  fixed-header
+                  hide-default-footer>
+      <template v-slot:item.actions="{ item }">
+        <v-btn icon color="indigo">
+          <v-icon>mdi-pen</v-icon>
+        </v-btn>
+        <v-btn icon color="error">
+          <v-icon>mdi-delete</v-icon>
+        </v-btn>
+      </template>
+    </v-data-table>
+
+  </v-card>
 </template>
 <script lang="ts">
 import {Component, Vue} from "vue-property-decorator"
 @Component({
   layout:"admin"
 })
-export default class Roles extends Vue {}
+export default class Roles extends Vue {
+  headers: any = [
+    { text: 'id',             value: 'id' },
+    { text: 'Название',       value: 'text' },
+    { text: 'Тех. обознач.',  value: 'role' },
+    { text: '',               value: 'actions' },
+  ]
+
+  roles: any = [
+    {
+      id: 1,
+      text: 'Админ',
+      role: 'admin'
+    },
+    {
+      id: 2,
+      text: 'Модератор',
+      role: 'moderator'
+    },
+    {
+      id: 3,
+      text: 'Пользователь',
+      role: 'user'
+    },
+    {
+      id: 4,
+      text: 'Директор уч',
+      role: 'director'
+    },
+    {
+      id: 5,
+      text: 'Заведующий уч',
+      role: 'manager'
+    },
+    {
+      id: 6,
+      text: 'Учитель',
+      role: 'teacher'
+    },
+    {
+      id: 7,
+      text: 'Учений',
+      role: 'student'
+    },
+    {
+      id: 8,
+      text: 'Работа разрешена',
+      role: 'jobs'
+    },
+    {
+      id: 9,
+      text: 'Доступ запрещён',
+      role: 'never'
+    },
+    {
+      id: 10,
+      text: 'Бан',
+      role: 'ban'
+    },
+  ]
+  //
+}
 </script>
