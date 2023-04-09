96a1b5305c63f566812ee17a8e2a2bf83877ece0
oleg <oleg_zub00@mail.ru>
2023-04-09 14:18:22 +0300

    users table visual

--- client/pages/admin/users.vue
+++ client/pages/admin/users.vue
@@ -1,30 +1,37 @@
 <template>
-    <v-card elevation="0">
-      <v-card-title>Пользователи</v-card-title>
-      <v-data-table :items="users"
-                    :headers="headers"
-                    :items-per-page="-1"
-                    class="custom-table"
-                    height="calc(100vh - 90px)"
-                    fixed-header
-                    hide-default-footer>
-        <template v-slot:item.user="{ item }">
-          {{item.name}} {{item.secondName}}
-        </template>
-        <template v-slot:item.activate="{ item }">
-            {{activate(item.activate)}}
-        </template>
-        <template v-slot:item.banStatus="{ item }">
-          {{banStatus(item.banStatus)}}
-        </template>
-        <template v-slot:item.actions="{ item }">
-          <v-btn icon color="indigo">
-            <v-icon>mdi-pen</v-icon>
-          </v-btn>
-          <admin-users-ban :item="item"/>
-        </template>
-      </v-data-table>
-    </v-card>
+  <v-card elevation="0">
+    <v-card-title>Пользователи</v-card-title>
+    <v-data-table :items="users"
+                  :headers="headers"
+                  :items-per-page="-1"
+                  class="custom-table"
+                  height="calc(100vh - 90px)"
+                  fixed-header
+                  hide-default-footer>
+      <template v-slot:item.user="{ item }">
+        {{item.name}} {{item.secondName}}
+      </template>
+      <template v-slot:item.bd="{ item }">
+        {{ $rest.getDate(item.bd) }}
+      </template>
+      <template v-slot:item.activate="{ item }">
+        <div :class="activate(item.activate).color">
+          {{ activate(item.activate).status }}
+        </div>
+      </template>
+      <template v-slot:item.banStatus="{ item }">
+        <div :class="banStatus(item.banStatus).color">
+          {{ banStatus(item.banStatus).status }}
+        </div>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-btn icon color="indigo">
+          <v-icon>mdi-pen</v-icon>
+        </v-btn>
+        <admin-users-ban :item="item"/>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 <script lang="ts">
 import {Component, Vue} from "vue-property-decorator"
@@ -74,11 +81,23 @@ export default class Users extends Vue {
   ]
 
   activate( value: boolean ){
-    return value ?'Актривирован(а)' : 'Не активирован(а)'
+    return value ? {
+      status: 'Актривирован(а)',
+      color: 'success--text'
+    } : {
+      status: 'Не активирован(а)',
+      color: 'warning--text'
+    }
   }
 
   banStatus( value: boolean ){
-    return value? 'Заблокирован(а)' : 'Нет блоикровок'
+    return value ? {
+      status: 'Заблокирован(а)',
+      color: 'error--text'
+    } : {
+      status: 'Нет блоикровок',
+      color: 'success--text'
+    }
   }
 
   changeBanStatus( value: boolean ){
