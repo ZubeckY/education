b4f6bc027a5f15b3fc17ccc4aedcb9e8c123d8bd
oleg <oleg_zub00@mail.ru>
2023-04-09 14:10:17 +0300

    edit user

---
+++ client/components/admin/users/edit.vue
@@ -0,0 +1,10 @@
+<template>
+
+</template>
+<script lang="ts">
+import {Component, Vue, Prop, Watch} from "vue-property-decorator"
+@Component
+export default class Ban extends Vue {
+  @Prop({required: true}) item!: any
+}
+</script>
