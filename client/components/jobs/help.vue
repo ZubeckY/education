79f04ed153a7f1c7a4e8ba13b87d27c7aab5a980
Yaroslav <yaroslavmetal94@gmail.com>
2023-04-22 23:27:34 +0300

    button!

--- client/components/jobs/help.vue
+++ client/components/jobs/help.vue
@@ -31,16 +31,40 @@ import {Component, Vue} from "vue-property-decorator"
 @Component
 export default class Jobs extends Vue {
   width: number = 60
-  height: number|string = 60
+  height: number = 60
 
   shadows:number = 0
   showText:boolean = false
   isActive:boolean = false
 
   switchText () {
-    this.showText = !this.showText
+    this.isActive = !this.isActive
     this.shadows = this.shadows === 0 ? 4: 0
+    if (!this.isActive){
+      this.height = 100
+      this.showText = !this.showText
+      let interval = setInterval(()=>{
+        this.width -= 15;
+        this.height -= 5;
+        if (this.width <= 60){
+          clearInterval(interval)
+          this.height = 60;
+          this.width = 60;
+        }
+      }, 10 )
+      return
+    }
+    let interval = setInterval(()=>{
+      this.width += 15;
+      this.height += 5;
+      if (this.width >= 250){
+        clearInterval(interval)
+        this.showText = !this.showText
+        this.height = 100
+      }
+    }, 10)
   }
+
 
 }
 </script>
