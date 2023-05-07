bfca0fd43e9dc1210ae11e63c92a1a5f421d7bb2
Yaroslav <yaroslavmetal94@gmail.com>
2023-05-07 22:32:54 +0300

    task-list

---
+++ client/pages/jobs/task-list.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <card-container>
+      <v-card
+        elevation="3"
+        outlined
+      >
+        <v-row>
+          <v-col cols="1">
+
+          </v-col>
+          <v-col>
+
+          </v-col>
+        </v-row>
+        <v-img src="https://ubiqum.com/assets/uploads/2019/10/screenshot-2021-02-11-at-115416.png"/>
+        <v-card-title>
+          12121
+        </v-card-title>
+        <v-card-subtitle>
+          12132324
+        </v-card-subtitle>
+        <v-card-text>
+
+        </v-card-text>
+        <v-card-actions>
+          actions
+        </v-card-actions>
+      </v-card>
+    </card-container>
+  </v-container>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator"
+@Component ({
+  layout: 'jobs'
+})
+export default class Jobs extends Vue {
+
+}
+</script>
+
